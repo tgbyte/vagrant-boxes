f7e9a40e0881beab4197ecbbe652885d49df6262
Stefan Reuter <stefan.reuter@reucon.com>
2013-11-30 22:04:40 +0100

    Remove deb file after installation

--- packer/debian-7.2.0-amd64/scripts/puppet.sh
+++ packer/debian-7.2.0-amd64/scripts/puppet.sh
@@ -3,3 +3,4 @@ wget http://apt.puppetlabs.com/puppetlabs-release-wheezy.deb
 dpkg -i puppetlabs-release-wheezy.deb
 apt-get update
 apt-get -y --force-yes install puppet
+rm http://apt.puppetlabs.com/puppetlabs-release-wheezy.deb
