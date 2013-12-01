08c9427b5ab8ed0a0156d8f9ec97b02712452024
Stefan Reuter <stefan.reuter@reucon.com>
2013-12-01 01:20:38 +0100

    Fix rm

--- packer/debian-7.2.0-amd64/scripts/puppet.sh
+++ packer/debian-7.2.0-amd64/scripts/puppet.sh
@@ -3,4 +3,4 @@ wget http://apt.puppetlabs.com/puppetlabs-release-wheezy.deb
 dpkg -i puppetlabs-release-wheezy.deb
 apt-get update
 apt-get -y --force-yes install puppet
-rm http://apt.puppetlabs.com/puppetlabs-release-wheezy.deb
+rm -f puppetlabs-release-wheezy.deb
